1a0a6ac0205e5f11598522038d2aaeaf6042b2c2
James-M-Fleming-985 <64785569+James-M-Fleming-985@users.noreply.github.com>
2025-11-24 13:09:36 +0000

    v1.0.170: Scale canvas to fit modal viewport
    - Add CSS to scale 1920x1080 canvas to 100% width
    - Maintain aspect ratio with height: auto
    - Add overflow: hidden to container
    - Canvas was rendering off-screen due to massive size
    - Logs show SVG loads successfully, just not visible

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from fastapi.responses import FileResponse
 
 # Build version - INCREMENT THIS BEFORE EACH DEPLOYMENT
 
-BUILD_VERSION = "1.0.169"  # Remove textBaseline property, use prototype
+BUILD_VERSION = "1.0.170"  # Scale canvas to fit modal viewport
 
 
 # Setup logging
