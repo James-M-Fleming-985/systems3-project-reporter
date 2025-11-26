9c2d960fe2eb09c22eb96b6638033c60ef30c25d
James-M-Fleming-985 <64785569+James-M-Fleming-985@users.noreply.github.com>
2025-11-26 12:02:00 +0000

    v1.0.200: Update BUILD_VERSION

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from fastapi.responses import FileResponse
 
 # Build version - INCREMENT THIS BEFORE EACH DEPLOYMENT
 
-BUILD_VERSION = "1.0.199"  # Fix Dockerfile CMD to use run.py not railway_start.py
+BUILD_VERSION = "1.0.200"  # Add missing run.py to git
 
 
 # Setup logging
