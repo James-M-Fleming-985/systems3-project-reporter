140071914d4edb3dde7684d4e62b93cc67fba39b
James-M-Fleming-985 <64785569+James-M-Fleming-985@users.noreply.github.com>
2025-12-15 12:25:09 +0000

    Rename Impact to Contingency, make editable with expandable textareas

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from fastapi.responses import FileResponse
 
 # Build version - INCREMENT THIS BEFORE EACH DEPLOYMENT
 
-BUILD_VERSION = "1.0.277"  # Fix regex_replace filter in dashboard router
+BUILD_VERSION = "1.0.278"  # Editable Contingency field, expandable textareas
 
 
 # Setup logging
