81112ffdf21a9b3af71dc7b283689e02808d98b0
James-M-Fleming-985 <64785569+James-M-Fleming-985@users.noreply.github.com>
2025-12-15 13:36:28 +0000

    Use div for Change ID to enable proper text wrapping - v1.0.283

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from fastapi.responses import FileResponse
 
 # Build version - INCREMENT THIS BEFORE EACH DEPLOYMENT
 
-BUILD_VERSION = "1.0.282"  # Fix word-break in Change ID column
+BUILD_VERSION = "1.0.283"  # Use div instead of span for Change ID wrapping
 
 
 # Setup logging
